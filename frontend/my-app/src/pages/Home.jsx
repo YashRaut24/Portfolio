66ec81b0fb6ae85d4aeb0b89afd6a6cdecfb1536
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-08 21:51:36 +0530

    Wired Book component into Home page

--- frontend/my-app/src/pages/Home.jsx
+++ frontend/my-app/src/pages/Home.jsx
@@ -1,9 +1,10 @@
+import Book from '../components/Book/Book';
 import './Home.css';
 
 function Home() {
   return (
     <div className="home-page">
-      <h1>Book goes here</h1>
+      <Book />
     </div>
   );
 }
