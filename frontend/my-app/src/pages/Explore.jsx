778055a8744f28bbbdc45a707dc48be30adf8107
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-10 09:34:34 +0530

    Wired header into explore page and adjusted hub spacing

--- frontend/my-app/src/pages/Explore.jsx
+++ frontend/my-app/src/pages/Explore.jsx
@@ -1,10 +1,12 @@
 import CircularHub from '../components/CircularHub/CircularHub';
 import ThemeToggle from '../components/ThemeToggle/ThemeToggle';
+import Header from '../components/Header/Header';
 import './Explore.css';
 
 function Explore() {
   return (
     <div className="explore-page">
+      <Header />
       <ThemeToggle />
       <CircularHub />
     </div>
