f0af8ee4192f5d57b858ce492b3715eab967ff6c
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-10 09:29:36 +0530

    Wired theme provider and toggle into explore page

--- frontend/my-app/src/main.jsx
+++ frontend/my-app/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@fontsource/kalam/400.css';
+import '@fontsource/kalam/700.css';
+import '@fontsource/comfortaa/400.css';
+import '@fontsource/comfortaa/700.css';
+import './styles/variables.css';
+import './styles/reset.css';
+import './styles/global.css';
+import { ThemeProvider } from './context/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
--- frontend/my-app/src/pages/Explore.jsx
+++ frontend/my-app/src/pages/Explore.jsx
@@ -1,9 +1,11 @@
 import CircularHub from '../components/CircularHub/CircularHub';
+import ThemeToggle from '../components/ThemeToggle/ThemeToggle';
 import './Explore.css';
 
 function Explore() {
   return (
     <div className="explore-page">
+      <ThemeToggle />
       <CircularHub />
     </div>
   );
