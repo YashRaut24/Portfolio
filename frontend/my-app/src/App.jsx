584d832bf7d0f7c6049887ac545dde0ddff6f66c
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-11 22:00:23 +0530

    Removed unused app css import

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Explore from './pages/Explore';
 import NotFound from './components/NotFound/NotFound';
-import './App.css';
 
 function App() {
   return (
