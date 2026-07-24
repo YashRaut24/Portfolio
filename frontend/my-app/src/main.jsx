98b64284cf7a654fa9a589bf4303b47462c18ff9
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-24 21:37:47 +0530

    Update main entry point

--- frontend/my-app/src/main.jsx
+++ frontend/my-app/src/main.jsx
@@ -9,12 +9,9 @@ import '@fontsource/comfortaa/700.css';
 import './styles/variables.css';
 import './styles/reset.css';
 import './styles/global.css';
-import { ThemeProvider } from './context/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <ThemeProvider>
         <App />
-      </ThemeProvider>
   </StrictMode>
 )
