f37eb013990bb5d05238ec51ad2dbb629dcb37de
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-29 23:43:55 +0530

    Update Experience component logic

--- frontend/my-app/src/components/Experience/Experience.jsx
+++ frontend/my-app/src/components/Experience/Experience.jsx
@@ -3,19 +3,34 @@ import './Experience.css';
 
 function Experience() {
   return (
-    <ul className="experience-list">
+    <div className="experience-list">
       {experienceData.map((item, index) => (
-        <li
+        <div
           key={index}
           className="experience-item stagger-item"
           style={{ '--stagger-index': index }}
         >
-          <span className="experience-date">{item.date}</span>
-          <h4 className="experience-title">{item.title}</h4>
-          <p className="experience-desc">{item.description}</p>
-        </li>
+          <div className="experience-header">
+            <h4 className="experience-title">{item.title}</h4>
+            <span className="experience-date">{item.date}</span>
+          </div>
+          
+          <div className="experience-company">
+            <span className="company-name">{item.company}</span>
+            <span className="company-separator">•</span>
+            <span className="company-type">{item.type}</span>
+          </div>
+
+          <ul className="experience-bullets">
+            {item.bullets.map((bullet, bulletIndex) => (
+              <li key={bulletIndex} className="experience-bullet">
+                {bullet}
+              </li>
+            ))}
+          </ul>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 }
 
