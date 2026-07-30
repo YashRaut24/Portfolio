e7211c529ef30df97379df496be0c189405770f1
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-30 19:36:42 +0530

    update experience data

--- frontend/my-app/src/data/experience.js
+++ frontend/my-app/src/data/experience.js
@@ -11,17 +11,17 @@ export const experienceData = [
       "Optimized media processing workflows by integrating Cloudinary and Multer, ensuring secure and efficient handling of user-generated images with sub-2-second load times to enhance UI/UX."
     ]
   },
-  {
-    date: "2024 - Present",
-    title: "Hackathon Participant & Prototyper",
-    company: "Various Competitions",
-    location: "Mumbai",
-    type: "Competition",
-    bullets: [
-      "GDG Mumbai (Build & Grow 2.0): Developed MedScribe-360, an AI-powered platform to capture and preserve medical conversations, improving documentation efficiency.",
-      "Code-A-Thon 2.0: Built EngageIQ, an ML-based social media analytics platform designed for scalable and efficient insights.",
-      "Abhirva 3.0 (NMIMS): Proposed a healthcare solution focused on assisting doctors with time-saving workflows and outbreak prediction.",
-      "Singularity Hackathon: Developed ZeroBite, a solution to enhance waste management through efficient tracking and optimization."
-    ]
-  }
+  // {
+  //   date: "2024 - Present",
+  //   title: "Hackathon Participant & Prototyper",
+  //   company: "Various Competitions",
+  //   location: "Mumbai",
+  //   type: "Competition",
+  //   bullets: [
+  //     "GDG Mumbai (Build & Grow 2.0): Developed MedScribe-360, an AI-powered platform to capture and preserve medical conversations, improving documentation efficiency.",
+  //     "Code-A-Thon 2.0: Built EngageIQ, an ML-based social media analytics platform designed for scalable and efficient insights.",
+  //     "Abhirva 3.0 (NMIMS): Proposed a healthcare solution focused on assisting doctors with time-saving workflows and outbreak prediction.",
+  //     "Singularity Hackathon: Developed ZeroBite, a solution to enhance waste management through efficient tracking and optimization."
+  //   ]
+  // }
 ];
