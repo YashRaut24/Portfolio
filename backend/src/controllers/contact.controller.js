9eb765dc1ed86ae6a5037953c8a6b1fd53dc1a50
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-30 19:36:38 +0530

    update contact controller logic

--- backend/src/controllers/contact.controller.js
+++ backend/src/controllers/contact.controller.js
@@ -1,7 +1,4 @@
-const express = require('express');
-const router = express.Router();
-
-module.exports = router;const { sendContactEmail } = require('../services/email.service');
+const { sendContactEmail } = require('../services/email.service');
 
 const submitContactForm = async (req, res, next) => {
   try {
@@ -9,7 +6,7 @@ const submitContactForm = async (req, res, next) => {
     await sendContactEmail({ name, email, message });
     res.status(200).json({ message: 'Message sent successfully' });
   } catch (error) {
-    next(error);
+    next(error); 
   }
 };
 
